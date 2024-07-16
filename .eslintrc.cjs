9e7ba3a603d73308c523e62b358060051b6518b9
JIMIN1020 <jimins4920@gmail.com>
2024-07-16 23:53:12 +0900

    [Chore]: EsLint, Prettier 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,67 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
   rules: {
-    'react-refresh/only-export-components': [
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': ['off'],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.ts', '.tsx'] }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      // 네이밍 컨벤션
       'warn',
-      { allowConstantExport: true },
+      {
+        selector: 'typeAlias', // 타입 선언
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'interface', // 인터페이스 선언
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'function', // exported function (컴포넌트 명)
+        format: ['PascalCase'],
+        modifiers: ['exported'],
+      },
+      {
+        selector: 'function', // function
+        format: ['camelCase'],
+      },
     ],
   },
-}
+};
