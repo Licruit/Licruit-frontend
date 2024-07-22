20aceae8457ddbad4eb8c59d5ec21ed90f84df8a
JIMIN1020 <jimins4920@gmail.com>
2024-07-22 23:22:16 +0900

    [Chore/#1]: ThemeProvider 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import GlobalStyle from '@/styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
 import Router from './routes';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  color: {
+    common: {
+      100: '#FFFFFF',
+    },
+    primary: {
+      500: '#FF4E08',
+    },
+    neutral: {
+      50: '#F6F8FB',
+      400: '#ADAEB1',
+      900: '#141517',
+    },
+  },
+  fontSize: {
+    base: '14px',
+  },
+} as const;
+
+export default theme;
