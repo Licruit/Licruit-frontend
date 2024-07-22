0699c6fa3f05abd78d92282ab651918d2a984200
JIMIN1020 <jimins4920@gmail.com>
2024-07-23 00:22:03 +0900

    [Style/#1]: Header 구현

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import Logo from 'public/assets/images/logo.svg?react';
+import { Link, useLocation } from 'react-router-dom';
+import PATH from '@/constants/path';
+
+function Header() {
+  const { pathname } = useLocation();
+
+  return (
+    <HeaderBar>
+      <div className='wrapper'>
+        <NavLink to={PATH.main}>
+          <Logo />
+        </NavLink>
+        <Nav $gap={36}>
+          <NavLink to={PATH.catalog}>TRADITIONAL LIQUOR SPACE</NavLink>
+          <NavLink to={PATH.group_buying}>GROUP BUYING</NavLink>
+        </Nav>
+      </div>
+      {!pathname.startsWith('/auth') && (
+        <Nav $gap={24}>
+          <NavLink to={PATH.login}>LOGIN</NavLink>
+          <NavLink to={PATH.join}>SIGN UP</NavLink>
+        </Nav>
+      )}
+    </HeaderBar>
+  );
+}
+
+export default Header;
+
+const HeaderBar = styled.header`
+  width: 100%;
+  padding: 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  position: sticky;
+  position: -webkit-sticky;
+  top: 0;
+  left: 0;
+
+  background: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(10px);
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    gap: 36px;
+  }
+`;
+
+const Nav = styled.nav<{ $gap: number }>`
+  display: flex;
+  gap: ${({ $gap }) => $gap}px;
+`;
+
+const NavLink = styled(Link)`
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: 500;
+`;
---
+++ src/constants/path.ts
@@ -0,0 +1,11 @@
+const PATH = {
+  login: '/auth/login',
+  join: '/auth/join',
+  find_password: '/auth/find-password',
+  main: '/',
+  catalog: '/catalog',
+  management: '/management',
+  group_buying: '/group-buying',
+} as const;
+
+export default PATH;
