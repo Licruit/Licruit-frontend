8de3db9d01aed3b156178c911df6445811a17275
JIMIN1020 <jimins4920@gmail.com>
2024-07-25 12:17:16 +0900

    [Refactor/#1]: formTitle 데이터 구조 변경

--- src/features/Auth/components/common/FormTitle.tsx
+++ src/features/Auth/components/common/FormTitle.tsx
@@ -3,7 +3,7 @@ import { FORM_TITLE } from '../../constants/formTitle';
 
 interface Props {
   type: keyof typeof FORM_TITLE;
-  step?: keyof (typeof FORM_TITLE)[keyof typeof FORM_TITLE];
+  step?: number;
 }
 
 function FormTitle({ type, step = 1 }: Props) {
@@ -12,7 +12,7 @@ function FormTitle({ type, step = 1 }: Props) {
   return (
     <Container>
       <h1>{data.title}</h1>
-      <p>{data[step]}</p>
+      <p>{data.step[step]}</p>
     </Container>
   );
 }
