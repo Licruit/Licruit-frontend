19ba546b1f4751a3be81bf49547f9d25a1887c73
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-07-27 05:35:52 +0900

    Revert "[refactor/#2]:코드 최적화"
    This reverts commit 06dcd3a0fddaf29a3b5a0befc11dfcc3084345fc.

--- src/features/Auth/components/Join/IndustryForm.tsx
+++ src/features/Auth/components/Join/IndustryForm.tsx
@@ -1,59 +1,10 @@
-import Button from '@/components/Button/Button';
-import FormInput from '@/components/Input/FormInput';
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
 import styled from 'styled-components';
 
 function IndustryForm() {
-  const { register } = useFormContext();
-  const handleAddress = () => {
-    // TODO: 서버 연동
-  };
-  return (
-    <Container>
-      <InputWithButton>
-        <FormInput
-          type='address'
-          placeholder='주소를 입력해주세요'
-          {...register('address', { required: true })}
-        />
-        <Button
-          type='button'
-          onClick={handleAddress}
-          $style='outlined'
-          $size='lg'
-          $theme='primary'
-        >
-          검색
-        </Button>
-      </InputWithButton>
-      <InputWrapper>
-        <FormInput
-          type='industry'
-          placeholder='상호명을 입력해주세요'
-          {...register('industry', { required: true })}
-        />
-      </InputWrapper>
-    </Container>
-  );
+  return <Container>a</Container>;
 }
 
 export default IndustryForm;
 
-const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-const InputWithButton = styled.div`
-  width: 100%;
-  display: flex;
-  gap: 10px;
-`;
-
-const InputWrapper = styled.div`
-  width: 100%;
-  position: relative;
-`;
+const Container = styled.div``;
