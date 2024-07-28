774f2a1c294361ece49174e6e5a9535c635fef4a
Juhee Choi <66045537+juhee067@users.noreply.github.com>
2024-07-28 20:30:57 +0900

    [chore/#2]:불필요한 try-catch 삭제

--- src/features/Auth/api/signup.api.ts
+++ src/features/Auth/api/signup.api.ts
@@ -2,18 +2,10 @@ import { httpClient } from '@/api/http';
 import { KSIC, Signup } from '../types/signup';
 
 export const signup = async (userData: Signup) => {
-  try {
-    await httpClient.post('/users/register', userData);
-  } catch (err) {
-    throw err;
-  }
+     await httpClient.post('/users/register', userData);
 };
 
 export const getKSIC = async (): Promise<KSIC[]> => {
-  try {
-    const response = await httpClient.get('/sectors');
+ const response = await httpClient.get('/sectors');
     return response.data;
-  } catch (err) {
-    throw err;
-  }
 };
