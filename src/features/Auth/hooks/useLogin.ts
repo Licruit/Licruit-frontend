d39fa36eaa8cb43fbe862a02e7df21919840d0e3
JIMIN1020 <jimins4920@gmail.com>
2024-07-29 01:53:38 +0900

    [Feat/#7]: session store 생성, public routes 적용

--- src/features/Auth/hooks/useLogin.ts
+++ src/features/Auth/hooks/useLogin.ts
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useSessionStore from '@/store/sessionStore';
 import { LoginForm } from '../types/login';
 import { login } from '../api/login.api';
 
 export const useLogin = () => {
   const navigate = useNavigate();
+  const setIsLoggedIn = useSessionStore((state) => state.setIsLoggedIn);
   const [isFailed, setIsFailed] = useState<boolean>(false);
 
   const handleLogin = async (value: LoginForm) => {
     login(value).then((res) => {
       if (res) {
+        setIsLoggedIn(true);
         navigate('/');
       } else {
         setIsFailed(true);
