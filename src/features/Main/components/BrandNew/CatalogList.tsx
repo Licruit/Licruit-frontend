4b4b8d8873e345f89e04e609c26be4be657f37ba
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-07-30 01:50:55 +0900

    [STYLE/18]: 카탈로그 리스트 UI 구현

---
+++ src/features/Main/components/BrandNew/CatalogList.tsx
@@ -0,0 +1,77 @@
+import Badge from '@/components/Badge/Badge';
+import styled from 'styled-components';
+import Title from '../common/Title';
+
+function CatalogList() {
+  return (
+    <CatalogListContainer>
+      <CatalogListItem>
+        <Badge $type='black' $size='sm'>
+          과실주
+        </Badge>
+        <Title $size='30' $color='common0'>
+          초선의 꿈
+        </Title>
+        <Description>
+          초선의 꿈 (로제)은 특별한 기념일에 함께 마시면 장밋빛 사랑이 솔솔
+          피어날 듯한 연분홍 장미색을 가진 와인으로, 풍부한 장미향과 상큼한 사과
+          향은 목 넘김을 부드럽게 해주고 감미롭고 달콤한 맛에 여성분들이
+          좋아하는 와인이다.
+        </Description>
+      </CatalogListItem>
+      <CatalogListItem>
+        <Badge $type='black' $size='sm'>
+          과실주
+        </Badge>
+        <Title $size='30' $color='common0'>
+          초선의 꿈
+        </Title>
+        <Description>
+          초선의 꿈 (로제)은 특별한 기념일에 함께 마시면 장밋빛 사랑이 솔솔
+          피어날 듯한 연분홍 장미색을 가진 와인으로, 풍부한 장미향과 상큼한 사과
+          향은 목 넘김을 부드럽게 해주고 감미롭고 달콤한 맛에 여성분들이
+          좋아하는 와인이다.
+        </Description>
+      </CatalogListItem>
+      <CatalogListItem>
+        <Badge $type='black' $size='sm'>
+          과실주
+        </Badge>
+        <Title $size='30' $color='common0'>
+          초선의 꿈
+        </Title>
+        <Description>
+          초선의 꿈 (로제)은 특별한 기념일에 함께 마시면 장밋빛 사랑이 솔솔
+          피어날 듯한 연분홍 장미색을 가진 와인으로, 풍부한 장미향과 상큼한 사과
+          향은 목 넘김을 부드럽게 해주고 감미롭고 달콤한 맛에 여성분들이
+          좋아하는 와인이다.
+        </Description>
+      </CatalogListItem>
+    </CatalogListContainer>
+  );
+}
+
+const CatalogListContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+const CatalogListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+
+  padding-bottom: 40px;
+
+  border-bottom: 1px solid ${({ theme }) => theme.color.neutral[400]};
+
+  cursor: pointer;
+`;
+
+const Description = styled.p`
+  ${({ theme }) => theme.typo.body.medium[14]};
+  color: ${({ theme }) => theme.color.neutral[600]};
+`;
+
+export default CatalogList;
