4e7b6741c82c70e2bc92c771e74c835ad8ee34c0
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-07-30 03:11:16 +0900

    [api/#3]:사업자번호 api 연동

--- src/features/Auth/api/signup.api.ts
+++ src/features/Auth/api/signup.api.ts
@@ -12,8 +12,11 @@ export const getKSIC = async (): Promise<KSIC[]> => {
   return response.data;
 };
 
-export const VerificationBusiness = async (businessNumber: string) => {
-  const data = { b_no: [businessNumber] };
+export const verificationBusiness = async (companyNumber: string) => {
+  const data = { b_no: [companyNumber] };
   const response = await axios.post(BUSINESS_URL, data);
-  return response.data;
+
+  if (response.data.data[0].b_stt !== '계속사업자') {
+    throw new Error();
+  }
 };
