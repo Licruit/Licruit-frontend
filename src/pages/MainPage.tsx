9c3eb731668bb22c48a1a276051eecc5dc8ef7d5
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-07-31 18:21:56 +0900

    [REFACTOR/#18]: Main 페이지 index 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,4 @@
-import BestSale from '@/features/Main/components/BestSale/BestSale';
-import BrandNew from '@/features/Main/components/BrandNew/BrandNew';
-import JoinUs from '@/features/Main/components/JoinUs/JoinUs';
-import Rate from '@/features/Main/components/Rate/Rate';
+import { BestSale, Rate, JoinUs, BrandNew } from '@/features/Main';
 
 import { IMAGES } from '@/constants/images';
 import { Banner } from '@/styles/components/Banner';
