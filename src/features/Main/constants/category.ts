657f529969cc3784d57bfefb4968c5c7e79e644c
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-01 01:07:03 +0900

    [STYLE/#18]: 메인 배너 이미지 및 제목 수정

--- src/features/Main/constants/category.ts
+++ src/features/Main/constants/category.ts
@@ -4,7 +4,7 @@ export const CATEGORY_TEXT = {
     description: '함께 공동구매로 전통주부터 인기 주류까지 저렴하게 만나보세요',
   },
   rate: {
-    title: '사장님들의 솔직 리뷰',
+    title: '사장님들의 술직 리뷰',
     description: '솔직한 사장님들의 리뷰 모음 ! 진짜 맛을 여기서 찾아보세요',
   },
   new: {
