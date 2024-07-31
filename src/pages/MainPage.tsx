2dd8e586b579b739688ad4682b7fd570629c5075
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-07-31 18:13:08 +0900

    [REFACTOR/#18]: Banner 컴포넌트 스타일 컴포넌트화

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,15 @@
-import Banner from '@/features/Main/components/Banner';
 import BestSale from '@/features/Main/components/BestSale/BestSale';
 import BrandNew from '@/features/Main/components/BrandNew/BrandNew';
 import JoinUs from '@/features/Main/components/JoinUs/JoinUs';
 import Rate from '@/features/Main/components/Rate/Rate';
 
+import { IMAGES } from '@/constants/images';
+import { Banner } from '@/styles/components/Banner';
+
 function MainPage() {
   return (
     <>
-      <Banner />
+      <Banner $imageUrl={IMAGES.banner} />
       <BestSale />
       <Rate />
       <JoinUs />
