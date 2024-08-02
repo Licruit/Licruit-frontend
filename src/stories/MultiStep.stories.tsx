f17877f24329d4e00d28f70346ca31f08bd8093a
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-08-02 22:43:08 +0900

    [refactor/#24]:chromatic.yml 수정

--- src/stories/MultiStep.stories.tsx
+++ src/stories/MultiStep.stories.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/function-component-definition */
+
 import MultiStep, { MultiStepProps } from '@/components/Form/MultiStep';
 import theme from '@/styles/theme';
 import { Meta, StoryFn } from '@storybook/react';
@@ -12,39 +14,17 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn<MultiStepProps> = (args) => (
-  <ThemeProvider theme={theme}>
-    <MultiStep {...args} />
-  </ThemeProvider>
-);
+const Template: StoryFn<MultiStepProps> = (args) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <MultiStep {...args} />
+    </ThemeProvider>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
   current: 1,
-  maxStep: 5,
-};
-
-export const StepTwo = Template.bind({});
-StepTwo.args = {
-  current: 2,
-  maxStep: 5,
-};
-
-export const StepThree = Template.bind({});
-StepThree.args = {
-  current: 3,
-  maxStep: 5,
-};
-
-export const StepFour = Template.bind({});
-StepFour.args = {
-  current: 4,
-  maxStep: 5,
-};
-
-export const Complete = Template.bind({});
-Complete.args = {
-  current: 5,
   maxStep: 5,
 };
 
