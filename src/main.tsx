b715a40cf670b937a5d402d6252c85275fdf1bc9
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-08-03 12:28:40 +0900

    [refactor/#29]:환경설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,22 @@ import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-if (import.meta.env.MODE === 'production') {
-  Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN_KEY,
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration(),
-    ],
-    tracePropagationTargets: [
-      'localhost',
-      '5173',
-      /^https:\/\/licruit-web.netlify.app\/.*/,
-    ],
-    tracesSampleRate: 1.0,
-    tracesSampler: () => 0,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  });
-}
+Sentry.init({
+  environment: 'production',
+  dsn: import.meta.env.VITE_SENTRY_DSN_KEY,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracePropagationTargets: [
+    'localhost',
+    '5173',
+    /^https:\/\/licruit-web.netlify.app\/.*/,
+  ],
+  tracesSampleRate: 1.0,
+  tracesSampler: () => 0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
