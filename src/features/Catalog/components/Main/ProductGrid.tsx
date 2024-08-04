e5cbfa86ce72cc5ea6ef17b9ccd8720a344250da
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-08-04 15:10:56 +0900

    [style/#33]:술정보 카드 컴포넌트 확장

--- src/features/Catalog/components/Main/ProductGrid.tsx
+++ src/features/Catalog/components/Main/ProductGrid.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import LiquorUrl from 'public/assets/images/main/mock-image1 38.svg';
+import styled from 'styled-components';
+import ProductCard from './ProductCard';
 
 function ProductGrid() {
-  return <div>ProductGrid</div>;
+  // 예시데이터
+  const liguorData = [
+    {
+      imageUrl: LiquorUrl,
+      badgeText: '탁주',
+      title: '홉미드',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+  ];
+
+  return (
+    <Container>
+      <ProductCard liquorInfo={liguorData[0]} />
+    </Container>
+  );
 }
 
 export default ProductGrid;
+
+const Container = styled.div``;
