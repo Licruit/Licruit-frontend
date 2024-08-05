359cc96d2059f278e24190dde20b2e1668fe6ad1
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-05 15:40:23 +0900

    [STYLE/#38]: 모달을 위한 z-index 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -35,7 +35,7 @@ const HeaderBar = styled.header`
   position: sticky;
   top: 0;
   left: 0;
-  z-index: 999;
+  z-index: 99;
 
   background: rgba(255, 255, 255, 0.6);
   backdrop-filter: blur(10px);
