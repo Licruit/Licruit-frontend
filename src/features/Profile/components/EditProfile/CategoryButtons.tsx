7a8ac205475868cd43fa2388048eb2da7074998b
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-05 16:47:50 +0900

    [REFACTOR/#38]: 카테고리 상수화에 따른 변경

--- src/features/Profile/components/EditProfile/CategoryButtons.tsx
+++ src/features/Profile/components/EditProfile/CategoryButtons.tsx
@@ -2,25 +2,14 @@ import Button from '@/components/Button/Button';
 import styled from 'styled-components';
 import Label from '../common/Label';
 import { LABEL } from '../../constants/label';
-
-const category = [
-  '한식',
-  '일식',
-  '양식',
-  '중식',
-  '이자카야',
-  '요리주점',
-  '레스토랑',
-  '도매업체',
-  '기타',
-];
+import CATEGORY from '../../constants/category';
 
 function CategoryButtons() {
   return (
     <CategoryWrapper>
       <Label label={LABEL.category} isRequired />
       <CategoryButtonWrapper>
-        {category.map((item) => {
+        {CATEGORY.map((item) => {
           return (
             <Button $style='outlined' $theme='neutral' $size='sm' $transparent>
               {item}
