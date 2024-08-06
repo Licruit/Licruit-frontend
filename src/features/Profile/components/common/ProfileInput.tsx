4c79eec2be9417dc0a2f63a218408b324888df9d
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-07 02:04:12 +0900

    [STYLE/#43]: ProfileInput 검색 아이콘 및 인포 아이콘 추가

--- src/features/Profile/components/common/ProfileInput.tsx
+++ src/features/Profile/components/common/ProfileInput.tsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
+import { GlassesIcon, InfoIcon } from 'public/assets/icons';
 import Label from './Label';
 
 interface Props {
@@ -6,6 +7,8 @@ interface Props {
   placeholder: string;
   isRequired?: boolean;
   hasValidation?: boolean;
+  isSearch?: boolean;
+  hasInfo?: boolean;
 }
 
 function ProfileInput({
@@ -13,17 +16,33 @@ function ProfileInput({
   isRequired,
   placeholder,
   hasValidation,
+  isSearch = false,
+  hasInfo,
 }: Props) {
+  const theme = useTheme();
+
   return (
     <InputWrapper>
-      <Label label={label} isRequired={isRequired} />
-      <Input type='text' placeholder={placeholder} />
+      <LabelWrapper>
+        <Label label={label} isRequired={isRequired} />
+        {hasInfo && (
+          <InfoIcon
+            fill={theme.color.neutral[400]}
+            style={{ cursor: 'pointer' }}
+          />
+        )}
+      </LabelWrapper>
+      <IconWrapper>
+        {isSearch && <GlassesIcon fill={theme.color.neutral[400]} />}
+        <Input type='text' placeholder={placeholder} $isSearch={isSearch} />
+      </IconWrapper>
       {hasValidation && <TypeNumber>3/25</TypeNumber>}
     </InputWrapper>
   );
 }
 
 const InputWrapper = styled.div`
+  width: 100%;
   position: relative;
 
   display: flex;
@@ -31,12 +50,37 @@ const InputWrapper = styled.div`
   gap: 10px;
 `;
 
-const Input = styled.input`
-  padding: 17px 18px;
+const LabelWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+`;
+
+const Input = styled.input<{ $isSearch: boolean }>`
+  flex: 1;
+  padding: ${({ $isSearch }) =>
+    $isSearch ? '17px 18px 17px 42px ;' : '17px 18px;'};
+
   border: 0.8px solid ${({ theme }) => theme.color.neutral[400]};
 
   ${({ theme }) => theme.typo.body.medium[14]};
-  color: ${({ theme }) => theme.color.neutral[400]};
+  &::placeholder {
+    color: ${({ theme }) => theme.color.neutral[400]};
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: 100%;
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+
+  svg {
+    position: absolute;
+    left: 18px;
+  }
 `;
 
 const TypeNumber = styled.div`
