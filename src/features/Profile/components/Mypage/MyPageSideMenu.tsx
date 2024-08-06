631e05c5c09dbc5677df32097d539200757294aa
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-07 00:29:14 +0900

    [STYLE/#43]: 마이페이지 사이드 메뉴 컨테이너 컴포넌트 구현

---
+++ src/features/Profile/components/Mypage/MyPageSideMenu.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import useMyPageSideMenuStore from '@/store/mypageSideMenuStore';
+import MyPage from './MyPage';
+import EditProfile from '../EditProfile/EditProfile';
+
+interface Props {
+  onClose: () => void;
+}
+
+function MyPageSideMenu({ onClose }: Props) {
+  const content = useMyPageSideMenuStore((state) => state.content);
+
+  const closeMyPage = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <Overlay onClick={closeMyPage}>
+      <Container>
+        {content === 'my-page' && <MyPage onClose={onClose} />}
+        {content === 'edit-profile' && <EditProfile />}
+      </Container>
+    </Overlay>
+  );
+}
+
+const Overlay = styled.div`
+  width: 100%;
+  height: 100%;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+
+  background: rgba(0, 0, 0, 0.4);
+`;
+
+const Container = styled.div`
+  width: 500px;
+  height: 100%;
+
+  padding: 20px;
+
+  position: absolute;
+  right: 0;
+  z-index: 9999;
+
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  overflow: scroll;
+
+  background: ${({ theme }) => theme.color.common[100]};
+`;
+
+export default MyPageSideMenu;
