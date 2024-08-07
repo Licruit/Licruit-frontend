570eb484141d3a190c6b17fd37cd6c7ab1a94495
JIMIN1020 <jimins4920@gmail.com>
2024-08-07 15:23:31 +0900

    [Style/#45]: 공동구매 요청/취소 시 버튼 스타일링

--- src/features/CatalogDetail/components/SideBar.tsx
+++ src/features/CatalogDetail/components/SideBar.tsx
@@ -1,17 +1,18 @@
-import styled from 'styled-components';
-import { WavingIcon } from 'public/assets/icons';
+import styled, { useTheme } from 'styled-components';
+import { CheckIcon, WavingIcon } from 'public/assets/icons';
 import Button from '@/components/Button/Button';
 import GroupBuyingList from './GroupBuyingList/GroupBuyingList';
 import { useLiquorDetail } from '../hooks/useLiquorDetail';
 import { useRegister } from '../hooks/useRegister';
 
 function SideBar() {
+  const theme = useTheme();
   const { liquorDetail } = useLiquorDetail();
   const { handleRegister } = useRegister();
 
   if (!liquorDetail) return null;
 
-  const { name, liked } = liquorDetail;
+  const { name, liked, likes } = liquorDetail;
 
   return (
     <Container>
@@ -19,20 +20,18 @@ function SideBar() {
       <GroupBuyingList />
       <Button
         $theme='primary'
-        $style='solid'
+        $style={liked ? 'solid' : 'outlined'}
         $size='lg'
         $width='full'
         $disableHover={!!liked}
         onClick={() => handleRegister(!!liked)}
       >
         {liked ? (
-          '구매 신청 완료!'
+          <CheckIcon fill={theme.color.common[100]} width={18} height={18} />
         ) : (
-          <>
-            <WavingIcon fill='#FFFFFF' width={18} height={18} />
-            구매 신청하기 (현재 {liquorDetail.likes}명 신청했어요!)
-          </>
+          <WavingIcon fill={theme.color.primary[500]} width={18} height={18} />
         )}
+        {liked ? '구매 신청완료' : '구매 신청하기'} (현재 {likes}명 신청했어요!)
       </Button>
     </Container>
   );
