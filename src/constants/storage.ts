8553f89c5846555fcb4be89463078214e9ed5421
JIMIN1020 <jimins4920@gmail.com>
2024-08-07 14:09:49 +0900

    [Fix/#45]: 토큰 저장 방식 수정

---
+++ src/constants/storage.ts
@@ -0,0 +1,4 @@
+export const STORAGE_KEY = {
+  accessToken: '@LICRUIT_ACCESS_TOKEN',
+  refreshToken: '@LICRUIT_REFRESH_TOKEN',
+};
---
+++ src/features/Login/models/user.model.ts
@@ -0,0 +1,5 @@
+export interface LoginRes {
+  accessToken: string;
+  isWholesaler: boolean;
+  refreshToken: string;
+}
