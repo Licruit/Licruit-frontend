fad6a83ec74cd6df1d5aabf397957b377a78253d
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-08-08 14:24:02 +0900

    [refactor/#37]:hook form이 아닌 단일 input으로 변경

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,17 +1,22 @@
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
+import { useState } from 'react';
+import { GlassesIcon } from 'public/assets/icons';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { FormProvider, useForm } from 'react-hook-form';
-
-import FormInput from '../Input/FormInput';
+import { StyledInput } from '../Input/FormInput';
 
 function Search() {
+  const [search, setSearch] = useState<string>('');
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
+  const theme = useTheme();
   const navigate = useNavigate();
-  const { watch, register, handleSubmit } = useForm();
-  const search = watch('search');
-  const methods = useForm();
-  const onSubmit = () => {
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
     if (search) {
       searchParams.set('search', search);
     } else {
@@ -22,20 +27,37 @@ function Search() {
 
   return (
     <Container>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormInput
+      <form onSubmit={handleSubmit}>
+        <SearchBar>
+          <Glasses fill={theme.color.neutral[600]} />
+          <StyledInput
+            type='text'
+            value={search}
             placeholder='찾고 싶은 전통주를 입력하세요'
-            hasSearch
-            {...register('search')}
+            onChange={handleChange}
           />
-        </form>
-      </FormProvider>
+        </SearchBar>
+      </form>
     </Container>
   );
 }
 
 export default Search;
+
 const Container = styled.div`
   width: 260px;
+`;
+
+const SearchBar = styled.div`
+  position: relative;
+  input {
+    padding: 8px 8px 8px 36px;
+  }
+`;
+
+const Glasses = styled(GlassesIcon)`
+  position: absolute;
+  top: 50%;
+  left: 20px;
+  transform: translate(-50%, -50%);
 `;
