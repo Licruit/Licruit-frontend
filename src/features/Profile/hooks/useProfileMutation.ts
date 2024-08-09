ef01c318fc25549bf47edc6c09ead3bc0db3ffd5
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-09 17:16:53 +0900

    [REFACTOR/#58]: 텍스트 상수화 및 기타

--- src/features/Profile/hooks/useProfileMutation.ts
+++ src/features/Profile/hooks/useProfileMutation.ts
@@ -5,8 +5,7 @@ import { Profile } from '../model/profile.model';
 const useProfileMutation = () => {
   return useMutation({
     mutationFn: (data: Profile) => putProfile(data),
-    onError: (error) => {
-      console.log(error);
+    onError: () => {
       window.alert('잠시후 다시 시도해 주세요.');
     },
   });
