579c00a676fe71dccffcf78491626030a3f72832
JIMIN1020 <jimins4920@gmail.com>
2024-08-10 17:40:12 +0900

    [Chore/#60]: loadEnv 설정

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,32 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { VitePluginRadar } from 'vite-plugin-radar';
-import dotenv from 'dotenv';
 
-dotenv.config();
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgrPlugin(),
-    tsconfigPaths(),
-    VitePluginRadar({
-      analytics: {
-        id: process.env.VITE_GOOGLE_ANALYTICS_ID,
-      },
-    }),
-  ],
-  build: {
-    outDir: './dist',
-  },
-  test: {
-    include: ['src/test/*.test.ts?(x)'],
-    coverage: {
-      reporter: ['text', 'json-summary', 'json'],
+  return defineConfig({
+    plugins: [
+      react(),
+      svgrPlugin(),
+      tsconfigPaths(),
+      VitePluginRadar({
+        analytics: {
+          id: env.VITE_GOOGLE_ANALYTICS_ID,
+        },
+      }),
+    ],
+    build: {
+      outDir: './dist',
     },
-  },
-});
+    test: {
+      include: ['src/test/*.test.ts?(x)'],
+      coverage: {
+        reporter: ['text', 'json-summary', 'json'],
+      },
+    },
+  });
+};
