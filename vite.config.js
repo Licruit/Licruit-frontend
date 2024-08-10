3e716c5dac9216ccba372d8762bd8afbd56aba78
JIMIN1020 <jimins4920@gmail.com>
2024-08-10 17:31:43 +0900

    [Chore/#60]: ga 연동

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePluginRadar } from 'vite-plugin-radar';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  plugins: [react(), svgrPlugin(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgrPlugin(),
+    tsconfigPaths(),
+    VitePluginRadar({
+      analytics: {
+        id: process.env.VITE_GOOGLE_ANALYTICS_ID,
+      },
+    }),
+  ],
   build: {
     outDir: './dist',
   },
