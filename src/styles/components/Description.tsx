1b583a4e75c9fea19262821ffe2c4d31ddf66caf
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-12 15:32:36 +0900

    [FIX/#58]: br 태그 제거 및 white-space 속성 적용

--- src/styles/components/Description.tsx
+++ src/styles/components/Description.tsx
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export const SignOutDescription = styled.div`
   ${({ theme }) => theme.typo.body.semi_bold[14]};
   color: ${({ theme }) => theme.color.neutral[900]};
+
+  white-space: pre-line;
 `;
