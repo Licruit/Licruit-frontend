1f7813594e1bbfaab7f6c3ac2a43cfd60791b45e
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-08-12 18:42:39 +0900

    [refactor/#62]:useCatalog hook 안에서 params 처리

--- src/features/Catalog/hooks/useCatalog.ts
+++ src/features/Catalog/hooks/useCatalog.ts
@@ -1,9 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
+import { useLocation } from 'react-router-dom';
 import { getCatalog } from '../api/catalog.api';
 import { CatalogParams } from '../types/CatalogParams';
 import { CatalogItem } from '../types/catalog';
 
-export const useCatalog = (params: CatalogParams) => {
+export const useCatalog = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const page = searchParams.get('page') || '1';
+  const category = searchParams.get('category');
+  const search = searchParams.get('search');
+  const minAlcohol = searchParams.get('minAlcohol');
+  const maxAlcohol = searchParams.get('maxAlcohol');
+
+  const params: CatalogParams = {
+    page: Number(page),
+    category: category ? +category : undefined,
+    search: search || undefined,
+    minAlcohol: minAlcohol ? +minAlcohol : undefined,
+    maxAlcohol: maxAlcohol ? +maxAlcohol : undefined,
+  };
   const {
     data: catalogData,
     error,
