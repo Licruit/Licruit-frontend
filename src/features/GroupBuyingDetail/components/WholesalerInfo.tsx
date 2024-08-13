29431124047f642558233471a27e71ede5a590cb
JIMIN1020 <jimins4920@gmail.com>
2024-08-13 17:09:52 +0900

    [Style/#68]: 도매업체 프로필 컴포넌트

---
+++ src/features/GroupBuyingDetail/components/WholesalerInfo.tsx
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+
+function WholesalerInfo() {
+  return (
+    <Container>
+      <ProfileWrapper>
+        <h5>도매업체 소개</h5>
+        <Profile>
+          <img
+            src=''
+            alt='임시'
+            width={88}
+            height={88}
+            style={{ backgroundColor: 'lightgray' }}
+          />
+          <div className='intro-wrapper'>
+            <h4>도매업체명</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem
+              ipsum dolor sit amet, consectetur adipiscin
+            </p>
+          </div>
+        </Profile>
+      </ProfileWrapper>
+      <hr className='divider' />
+      <ExtraInfo>
+        <span>
+          홈페이지 : <a href='https://www.naver.com'>https://www.naver.com</a>
+        </span>
+        <span>누적 판매수 : 12,000병</span>
+      </ExtraInfo>
+    </Container>
+  );
+}
+
+export default WholesalerInfo;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid ${({ theme }) => theme.color.neutral[400]};
+  background-color: ${({ theme }) => theme.color.common[100]};
+  color: ${({ theme }) => theme.color.common[0]};
+
+  .divider {
+    border: 0;
+    width: 100%;
+    height: 1px;
+    margin: 0;
+    background: ${({ theme }) => theme.color.neutral[400]};
+  }
+`;
+
+const ProfileWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+
+  h5 {
+    ${({ theme }) => theme.typo.heading.bold[14]}
+  }
+`;
+
+const Profile = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: flex-end;
+
+  .intro-wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+
+    h4 {
+      ${({ theme }) => theme.typo.heading.bold[20]}
+    }
+
+    p {
+      ${({ theme }) => theme.typo.body.medium[14]}
+      color: ${({ theme }) => theme.color.neutral[400]};
+    }
+  }
+`;
+
+const ExtraInfo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 20px;
+  ${({ theme }) => theme.typo.body.medium[14]}
+  color: ${({ theme }) => theme.color.neutral[400]};
+
+  a {
+    text-decoration: underline;
+    color: ${({ theme }) => theme.color.primary[500]};
+  }
+`;
--- src/pages/GroupBuyingDetailPage.tsx
+++ src/pages/GroupBuyingDetailPage.tsx
@@ -1,5 +1,21 @@
+import WholesalerInfo from '@/features/GroupBuyingDetail/components/WholesalerInfo';
+import styled from 'styled-components';
+
 function GroupBuyingDetailPage() {
-  return <div>GroupBuyingDetailPage</div>;
+  return (
+    <Container>
+      <WholesalerInfo />
+      <div>info</div>
+    </Container>
+  );
 }
 
 export default GroupBuyingDetailPage;
+
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  padding-bottom: 80px;
+`;
