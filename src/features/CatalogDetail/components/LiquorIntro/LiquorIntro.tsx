47bc9bb7a02c421c6dcd8754915b7be37e982b57
JIMIN1020 <jimins4920@gmail.com>
2024-08-15 10:45:59 +0900

    [Fix/#68]: summary 제거

--- src/features/CatalogDetail/components/LiquorIntro/LiquorIntro.tsx
+++ src/features/CatalogDetail/components/LiquorIntro/LiquorIntro.tsx
@@ -1,30 +1,16 @@
 import Rating from '@/components/Rating/Rating';
-import { AiIcon } from 'public/assets/icons';
 import styled from 'styled-components';
 import StatBox from '@/components/StatBox/StatBox';
 
 function LiquorIntro() {
   return (
     <Container>
-      <div className='intro-wrapper'>
-        <Statistic>
-          <StatBox title='리뷰' iconType='review'>
-            <span className='review-count'>10</span>
-          </StatBox>
-          <StatBox title='평점' iconType='review'>
-            <Rating rating={4.5} hasLabel />
-          </StatBox>
-        </Statistic>
-        <Summary>
-          <AiIcon />
-          <p>
-            백경13은 부드럽고 깔끔한 맛으로, 다양한 음식과 완벽하게 어울립니다.
-            <br />
-            전통주의 깊은 맛과 현대적인 감각을 동시에 즐기고 싶다면, 지금 바로
-            백경13을 선택하세요!
-          </p>
-        </Summary>
-      </div>
+      <StatBox title='리뷰' iconType='review'>
+        <span className='review-count'>10</span>
+      </StatBox>
+      <StatBox title='평점' iconType='review'>
+        <Rating rating={4.5} hasLabel />
+      </StatBox>
     </Container>
   );
 }
@@ -32,49 +18,17 @@ function LiquorIntro() {
 export default LiquorIntro;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  width: 100%;
-
-  .intro-wrapper {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-
-    padding: 20px;
-
-    border: 0.8px solid ${({ theme }) => theme.color.neutral[400]};
-  }
-`;
-
-const Statistic = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  padding: 20px;
   width: 100%;
-  ${({ theme }) => theme.typo.body.medium[12]}
+  border: 0.8px solid ${({ theme }) => theme.color.neutral[400]};
   color: ${({ theme }) => theme.color.neutral[400]};
+  ${({ theme }) => theme.typo.body.medium[12]}
 
   .review-count {
     ${({ theme }) => theme.typo.body.semi_bold[16]}
     color: ${({ theme }) => theme.color.neutral[900]};
-  }
-`;
-
-const Summary = styled.div`
-  display: flex;
-  gap: 8px;
-  align-items: center;
-
-  padding: 20px;
-
-  color: ${({ theme }) => theme.color.primary[500]};
-
-  background-color: ${({ theme }) => theme.color.primary[100]};
-
-  p {
-    flex: 1;
-    ${({ theme }) => theme.typo.body.semi_bold[14]}
-    white-space: pre-wrap;
   }
 `;
