9e06a2f440ba3121fbd54772d22fa180c77dc81e
JIMIN1020 <jimins4920@gmail.com>
2024-08-19 19:42:37 +0900

    [Feat/#76]: 공동구매 참여 > optimistic update 적용

--- src/features/GroupBuyingDetail/hooks/useRegister.ts
+++ src/features/GroupBuyingDetail/hooks/useRegister.ts
@@ -1,7 +1,9 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import { useParams } from 'react-router-dom';
 import { registerGroupBuying } from '../api/groupBuying.api';
+import { GroupBuyingDetail } from '../models/groupBuyingDetail.model';
 
 interface BuyingForm {
   quantity: number;
@@ -9,6 +11,7 @@ interface BuyingForm {
 
 export const useRegister = () => {
   const { id } = useParams();
+  const queryClient = useQueryClient();
 
   const methods = useForm<BuyingForm>({
     mode: 'onChange',
@@ -20,8 +23,31 @@ export const useRegister = () => {
   const { mutate: handleRegister } = useMutation({
     mutationFn: ({ quantity }: BuyingForm) =>
       registerGroupBuying(Number(id), quantity),
-    onError: () => {
-      window.alert('오류가 발생했습니다. 다시 시도해주세요.'); // toast로 변경 예정
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: ['groupBuyingDetail', id] });
+
+      const prevData = queryClient.getQueryData<GroupBuyingDetail>([
+        'groupBuyingDetail',
+        id,
+      ]);
+
+      if (!prevData) {
+        return;
+      }
+
+      queryClient.setQueryData(['groupBuyingDetail', id], {
+        ...prevData,
+        isParticipated: !prevData.isParticipated,
+      });
+
+      return { prevData };
+    },
+    onError: (_err, _variable, context) => {
+      toast.error('오류가 발생했습니다.\n다시 시도해주세요.');
+      queryClient.setQueryData(['groupBuyingDetail', id], context?.prevData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['groupBuyingDetail', id] });
     },
   });
 
