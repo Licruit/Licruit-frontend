9c4a484d3ddbe1d61a54809593bf421acd12fe28
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-24 06:06:39 +0900

    [STYLE/#96]: textarea placeholder 폰트 및 색상 적용

--- src/features/SideMenu/components/Review/ReviewForm.tsx
+++ src/features/SideMenu/components/Review/ReviewForm.tsx
@@ -78,6 +78,10 @@ const Introduce = styled.textarea`
   padding: 17px 0 0 18px;
 
   border: 0.8px solid ${({ theme }) => theme.color.neutral[400]};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.neutral[400]};
+  }
 `;
 
 const Notice = styled.p`
