ca469cd4a0fde61a6504994fc5af4181984f9798
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-24 06:41:01 +0900

    [FEAT/#96]: review를 위한 store에 id 추가

--- src/store/mypageSideMenuStore.ts
+++ src/store/mypageSideMenuStore.ts
@@ -2,14 +2,27 @@ import { create } from 'zustand';
 
 type Step = 'my-page' | 'edit-profile' | 'signout' | 'review' | 'group-buying';
 
-interface States {
+interface MyPageSideMenuStates {
   content: Step;
-  setContent: (value: Step) => void;
+  id: number | null;
+  setContent: (content: Step, id?: number | null) => void;
 }
 
-const useMyPageSideMenuStore = create<States>((set) => ({
+interface MyPageIsOpenStates {
+  isOpen: boolean;
+  open: () => void;
+  close: () => void;
+}
+
+export const useMyPageSideMenuStore = create<MyPageSideMenuStates>((set) => ({
   content: 'my-page',
-  setContent: (updatedContent) => set({ content: updatedContent }),
+  id: null,
+  setContent: (updatedContent: Step, updatedId?: number | null) =>
+    set({ content: updatedContent, id: updatedId }),
 }));
 
-export default useMyPageSideMenuStore;
+export const useMyPageIsOpenStore = create<MyPageIsOpenStates>((set) => ({
+  isOpen: false,
+  open: () => set({ isOpen: true }),
+  close: () => set({ isOpen: false }),
+}));
