e305c6d5bc07bf0becae246d464b6be1e042306c
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-24 05:38:18 +0900

    [FEAT/#97]: 소상공인 마이페이지 인 경우만 호출

--- src/features/Profile/hooks/useGroupBuyListQuery.ts
+++ src/features/Profile/hooks/useGroupBuyListQuery.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import getGroupBuyList from '../api/getGroupBuyList';
 
-const useGroupBuyListQuery = () => {
+const useGroupBuyListQuery = (isCompany: boolean) => {
   const { data } = useQuery({
     queryKey: ['group-buy-list'],
     queryFn: getGroupBuyList,
+    enabled: !isCompany,
   });
 
   return { data };
