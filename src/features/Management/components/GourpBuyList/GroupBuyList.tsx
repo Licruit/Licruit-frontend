a5743e5c3743fef7a0655485f07b18616cf7b45d
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-27 05:35:50 +0900

    [FEAT/#127]: 공동구매 아이템 클릭시 상세페이지 이동 구현

--- src/features/Management/components/GourpBuyList/GroupBuyList.tsx
+++ src/features/Management/components/GourpBuyList/GroupBuyList.tsx
@@ -2,42 +2,56 @@ import Button from '@/components/Button/Button';
 import Pagination from '@/components/Pagination/Pagination';
 import { Badge } from '@/styles/components/Badge';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import PATH from '@/constants/path';
 import useCompanyGroupBuyListQuery from '../../hooks/useCompanyGroupBuyListQuery';
 
 function GroupBuyList() {
   const { companyGroupBuyList } = useCompanyGroupBuyListQuery();
+  const navigate = useNavigate();
 
   if (!companyGroupBuyList) return <></>;
 
+  const isEmpty = companyGroupBuyList.buyings.length === 0;
+
   return (
     <>
-      <ContentList>
-        {companyGroupBuyList.buyings.map((item) => (
-          <ContentItem>
-            <ContentImg $imageUrl={item.liquorImg} />
-            <ContentInfo>
-              <Badge $size='sm' $type='black'>
-                {item.leftDate}일 남음
-              </Badge>
-              <h3 className='title'>{item.title}</h3>
-              <p className='description'>{item.content}</p>
-            </ContentInfo>
-            <Button
-              $style='outlined'
-              $theme='neutral'
-              $size='sm'
-              $width='full'
-              $transparent
-            >
-              자세히 보기
-            </Button>
-          </ContentItem>
-        ))}
-      </ContentList>
-      <Pagination
-        totalItems={companyGroupBuyList.pagination.totalPage}
-        currentPage={companyGroupBuyList.pagination.currentPage}
-      />
+      {isEmpty ? (
+        <Notice>
+          <h1 className='notice'>공동구매 목록이 없습니다</h1>
+        </Notice>
+      ) : (
+        <>
+          <ContentList>
+            {companyGroupBuyList?.buyings.map((item) => (
+              <ContentItem key={item.id}>
+                <ContentImg $imageUrl={item.liquorImg} />
+                <ContentInfo>
+                  <Badge $size='sm' $type='black'>
+                    {item.leftDate}일 남음
+                  </Badge>
+                  <h3 className='title'>{item.title}</h3>
+                  <p className='description'>{item.content}</p>
+                </ContentInfo>
+                <Button
+                  $style='outlined'
+                  $theme='neutral'
+                  $size='sm'
+                  $width='full'
+                  $transparent
+                  onClick={() => navigate(`${PATH.management}/${item.id}`)}
+                >
+                  자세히 보기
+                </Button>
+              </ContentItem>
+            ))}
+          </ContentList>
+          <Pagination
+            totalItems={companyGroupBuyList?.pagination.totalPage}
+            currentPage={companyGroupBuyList?.pagination.currentPage}
+          />
+        </>
+      )}
     </>
   );
 }
@@ -50,8 +64,6 @@ const ContentList = styled.ul`
 `;
 
 const ContentItem = styled.li`
-  cursor: pointer;
-
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -86,6 +98,20 @@ const ContentInfo = styled.div`
     ${({ theme }) => theme.typo.body.medium[14]};
     color: ${({ theme }) => theme.color.neutral[400]};
     text-overflow: ellipsis;
+  }
+`;
+
+const Notice = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 40vh;
+
+  .notice {
+    ${({ theme }) => theme.typo.body.semi_bold[16]}
+    color: ${({ theme }) => theme.color.neutral[500]}
   }
 `;
 
