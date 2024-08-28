1e1dac1324b197079049fbd1861815cc42bb4172
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-28 16:36:31 +0900

    [STYLE/#140]: 공동구매 목록 페이지 스피너 적용

--- src/pages/ManagementPage.tsx
+++ src/pages/ManagementPage.tsx
@@ -1,5 +1,5 @@
+import LoadingSpinner from '@/features/Catalog/components/Spinner';
 import { CategoryTab, GroupBuyList } from '@/features/Management';
-import { Spinner } from '@/styles/components/Spinner';
 import { Suspense } from 'react';
 import styled from 'styled-components';
 
@@ -7,13 +7,7 @@ function ManagementPage() {
   return (
     <Container>
       <CategoryTab />
-      <Suspense
-        fallback={
-          <SpinnerWrapper>
-            <StyledSpinner />
-          </SpinnerWrapper>
-        }
-      >
+      <Suspense fallback={<LoadingSpinner />}>
         <GroupBuyList />
       </Suspense>
     </Container>
@@ -25,22 +19,6 @@ const Container = styled.div`
   flex-direction: column;
   gap: 20px;
   width: 100%;
-`;
-
-const SpinnerWrapper = styled.div`
-  position: relative;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  width: 100%;
-  height: 40vh;
-`;
-
-const StyledSpinner = styled(Spinner)`
-  width: 80px;
-  height: 80px;
 `;
 
 export default ManagementPage;
