64e9a738d96ba9163cc2129d6f673bfaa848b0ba
JIMIN1020 <jimins4920@gmail.com>
2024-08-29 00:11:31 +0900

    [Feat/#120]: 전통주 상세 > suspense, boundary 적용

--- src/features/LiquorDetail/hooks/useLiquorDetail.ts
+++ src/features/LiquorDetail/hooks/useLiquorDetail.ts
@@ -1,12 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { getLiquorDetail } from '../api/liquor.api';
 import { LiquorDetail } from '../models/liquor.model';
 
 export const useLiquorDetail = (id: number | undefined) => {
-  const { data } = useQuery<LiquorDetail>({
+  const { data } = useSuspenseQuery<LiquorDetail>({
     queryKey: ['liquorDetail', id],
     queryFn: () => getLiquorDetail(Number(id!)),
-    enabled: id !== undefined,
   });
 
   return { liquorDetail: data, liquorImg: data?.img };
