626279861977535124351867a023d16b3bbde66b
JIMIN1020 <jimins4920@gmail.com>
2024-08-29 21:32:08 +0900

    [Fix/#158]: 관리 목록 > 검색창 제거

--- src/features/Management/index.ts
+++ src/features/Management/index.ts
@@ -2,6 +2,5 @@ export { useBuyerList } from './hooks/useBuyerList';
 export { default as ProductCardWithButton } from './components/ProductCardWithButton';
 export { default as BuyerList } from './components/BuyerList/BuyerList';
 export { default as BuyDetail } from './components/BuyerDetail/BuyDetail';
-export { default as SearchBar } from './components/SearchBar';
 export { default as CategoryTab } from './components/GourpBuyList/CategoryTab';
 export { default as GroupBuyList } from './components/GourpBuyList/GroupBuyList';
